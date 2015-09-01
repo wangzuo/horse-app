20b7f3a32b21a32bdfa31a01c347c90f127b4bf2
Wang Zuo <wzuoadjusted@gmail.com>
2015-09-01 14:25:35 +0800

    0.0.1

---
+++ example.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = require('./');
+
+var app = express();
+
+app.use('/example', router({
+  cacheDir: __dirname + '/cached'
+}));
+
+app.listen(3991, function() {
+  console.log('listening on 3991');
+});
+
