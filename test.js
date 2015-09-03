1b4cac7a797e5ce63d933ffcb1238192369c127e
wang zuo <wzuoadjusted@gmail.com>
2015-09-03 23:43:36 +0800

    update test

--- test.js
+++ test.js
@@ -5,9 +5,21 @@ var app = require('./app');
 describe('GET /logo', function() {
   this.timeout(5000);
 
-  it('search with keywords', function(done) {
+  it('search with keywords from itunes', function(done) {
     request(app)
       .get('/horse/logo?key=chrome&market=itunes')
+      .expect(200, done);
+  });
+
+  it('search with keywords from googleplay', function(done) {
+    request(app)
+      .get('/horse/logo?key=chrome&market=googleplay')
+      .expect(200, done);
+  });
+
+  it('search with keywords from wandoujia', function(done) {
+    request(app)
+      .get('/horse/logo?key=chrome&market=wandoujia')
       .expect(200, done);
   });
 });
