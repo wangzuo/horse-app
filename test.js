ce9dd8dbf82b172ccb574302d678a3118b2e3f5e
wang zuo <wzuoadjusted@gmail.com>
2015-09-03 19:02:57 +0800

    req.query.market and request test

--- test.js
+++ test.js
@@ -1,6 +1,18 @@
-var test = require('tape');
+var express = require('express');
+var request = require('supertest');
 var router = require('./');
-var markets = require('./lib/markets');
+var app = express();
 
-test('itunes', function(t) {
+app.use('/horse', router({
+  cacheDir: __dirname + '/cached'
+}));
+
+describe('GET /logo', function() {
+  this.timeout(5000);
+
+  it('search with keywords', function(done) {
+    request(app)
+      .get('/horse/logo?key=chrome&market=itunes')
+      .expect(200, done);
+  });
 });
