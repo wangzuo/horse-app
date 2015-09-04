f0c5f5458eff734d72051bbafacab4a5f816aa54
Wang Zuo <wzuoadjusted@gmail.com>
2015-09-04 18:36:06 +0800

    /info -> /search

--- test.js
+++ test.js
@@ -23,3 +23,31 @@ describe('GET /logo', function() {
       .expect(200, done);
   });
 });
+
+describe('GET /search', function() {
+  this.timeout(10000);
+
+  it('search with keywords from itunes', function(done) {
+    request(app)
+      .get('/horse/search?key=chrome&market=itunes')
+      .expect(200, done);
+  });
+
+  it('search with keywords from googleplay', function(done) {
+    request(app)
+      .get('/horse/search?key=chrome&market=googleplay')
+      .expect(200, done);
+  });
+
+  it('search with keywords from wandoujia', function(done) {
+    request(app)
+      .get('/horse/search?key=chrome&market=wandoujia')
+      .expect(200, done);
+  });
+});
+
+describe('GET /app/:appId', function() {
+});
+
+describe('GET /logo/:appId', function() {
+});
